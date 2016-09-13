31abbb9f1a84cd5d4e625c04248b85ca77db51af
Yiqing Jin <yiqing_jin@apple.com>
2016-09-13 16:43:42 -0700

    format changes as requested by Rob.

--- osmtm/views/project.py
+++ osmtm/views/project.py
@@ -344,9 +344,13 @@ def project_comments(request):
     filename = '{}.csv'.format(project.name)
     request.response.content_disposition = 'attachment;filename=' + filename
 
-    header = ['Task', 'Author', 'Comment', 'Grid']
-    rows = [[t.id, t.comments[0].author.username if t.comments else '',
-             '\n'.join([c.comment for c in t.comments]), shape.to_shape(t.geometry).wkt] for t in tasks]
+    header = ['Time', 'Task', 'Author', 'Comment', 'Grid']
+    rows = [[c.date, t.id, c.author.username, c.comment, shape.to_shape(t.geometry).wkt] for t in tasks for c in
+            t.comments]
+    # rows = [[t.date, t.id, t.comments[0].author.username if t.comments else '',
+    #          '\n\n'.join([c.comment for c in sorted(t.comments, key=lambda k: k.date)]), shape.to_shape(t.geometry).wkt]
+    #         for t in tasks]
+
     return {
         'header': header,
         'rows': rows
